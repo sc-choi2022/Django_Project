1f1e83ba0a2da921da498dd8c2abd39ba57210fb
leeda1231 <dlekdls1231@gmail.com>
2022-05-20 21:18:05 +0900

    models.py

--- movie_data/django/accounts/models.py
+++ movie_data/django/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from movies.models import Movie
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    users_mymovie = models.ManyToManyField(Movie, related_name='movies_mymovie')
+    users_wish = models.ManyToManyField(Movie, related_name='movies_wish')
--- movie_data/django/movies/models.py
+++ movie_data/django/movies/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.conf import settings
 
 class Genre(models.Model):
     name = models.CharField(max_length=50)
@@ -33,5 +32,3 @@ class Movie(models.Model):
     video = models.CharField(max_length=100)
     vote_average = models.FloatField()
     vote_count = models.IntegerField()
-    mymovie_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='mymovie_movies')
-    wish_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='wish_movies')
