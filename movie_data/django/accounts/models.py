586360ee3003f7fa1a571b59b88165d9bd9c45d6
leeda1231 <dlekdls1231@gmail.com>
2022-05-20 17:20:49 +0900

    UserModel clash

--- movie_data/django/accounts/models.py
+++ movie_data/django/accounts/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from movies.models import Movie
+
+class Mymovie(models.Model):
+    title = models.CharField(max_length=100)
+
+class Wish(models.Model):
+    title = models.CharField(max_length=100)
 
 class User(AbstractUser):
-    pass
+    mymovies = models.ManyToManyField(Movie, related_name='users')
+    wishes = models.ManyToManyField(Movie, related_name='users')
