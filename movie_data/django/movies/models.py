61037a62da67b3cb9603d487a6079f16f07a53f4
최수연 <martin0125@naver.com>
2022-05-21 16:04:05 +0900

    final_model.py

--- movie_data/django/movies/models.py
+++ movie_data/django/movies/models.py
@@ -1,34 +1,37 @@
 from django.db import models
 
-class Genre(models.Model):
-    name = models.CharField(max_length=50)
-
-class Certification(models.Model):
-    name = models.CharField(max_length=50)
-
-class Actor(models.Model):
-    name = models.CharField(max_length=50)
-    profile_path = models.CharField(max_length=200)
-
-class Director(models.Model):
-    name = models.CharField(max_length=50)
-
-class OTT(models.Model):
-    name = models.CharField(max_length=50)
-
-
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     orginal_title = models.CharField(max_length=100)
     overview = models.TextField()
     poster_path = models.CharField(max_length=200)
     release_date = models.DateTimeField()
-    certification = models.ManyToManyField(Certification, related_name='movies')
     runtime = models.IntegerField()
-    genres = models.ManyToManyField(Genre, related_name='movies')
-    actors = models.ManyToManyField(Actor, related_name='movies')
-    directors = models.ManyToManyField(Director, related_name='movies')
-    otts =  models.ManyToManyField(OTT, related_name='movies')
     video = models.CharField(max_length=100)
     vote_average = models.FloatField()
     vote_count = models.IntegerField()
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    movies = models.ManyToManyField(Movie, related_name='genres')
+
+class Certification(models.Model):
+    name = models.CharField(max_length=50)
+    movies = models.ManyToManyField(Movie, related_name='certifications')
+
+class Actor(models.Model):
+    name = models.CharField(max_length=50)
+    profile_path = models.CharField(max_length=200)
+    movies = models.ManyToManyField(Movie, related_name='actors')
+
+class Director(models.Model):
+    name = models.CharField(max_length=50)
+    movies = models.ManyToManyField(Movie, related_name='directors')
+
+class OTT(models.Model):
+    name = models.CharField(max_length=50)
+    movies =  models.ManyToManyField(Movie, related_name='otts')
+
+class Keyword(models.Model):
+    name = models.CharField(max_length=50)
+    movies = models.ManyToManyField(Movie, related_name='keywords')
--- movie_data/django/movies/views.py
+++ movie_data/django/movies/views.py
@@ -13,30 +13,11 @@ def movie(request):
     params = {
         'api_key': API_KEY,
         'language': 'ko',
-        'page': 17,
+        'page': 1,
     }
     response = requests.get(BASE_URL+path, params=params)
     data = response.json()['results']
     for d in data:
-        movie = Movie()
-        movie.title = d['title']
-        movie.orginal_title = d['original_title']
-        if d['overview'] == '':
-            break
-        movie.overview = d['overview']
-        movie.poster_path = d['poster_path']
-        movie.vote_average = d['vote_average']
-        movie.vote_count = d['vote_count']
-        # genres = d['genre_ids']
-        # for genre in genres:
-        #     mm = {28: '액션'}
-        #     g1 = Genre(pk=genre, name=mm[genre])
-        #     g1.save()
-        #     movie.genres.add(g1)
-        # movie.genres = d['genre_ids']
-        movie.release_date = d['release_date']
-        if d['release_date'] == '':
-            continue
         id = d['id']
         path1 = '/movie/' + str(id)
         params1 = {
@@ -46,12 +27,20 @@ def movie(request):
         }
         response1 = requests.get(BASE_URL+path1, params=params1)
         data1 = response1.json()
+        movie = Movie()
+        movie.title = data1['title']
+        movie.orginal_title = data1['original_title']
+        if data1['overview'] == '':
+            continue
+        movie.overview = data1['overview']
+        movie.poster_path = data1['poster_path']
+        if d['release_date'] == '':
+            continue
+        movie.release_date = data1['release_date']
         movie.runtime = data1['runtime']
-        certifi = data1['releases']['countries']
-        for cer in certifi:
-            if cer['iso_3166_1'] == 'KR':
-                movie.certification_set = cer['certification']
-                break
+        movie.vote_average = data1['vote_average']
+        movie.vote_count = data1['vote_count']
+        
         path2 = path1 + '/videos'
         params2 = {
             'api_key': API_KEY,
@@ -67,21 +56,11 @@ def movie(request):
                 break
         if movie.video == '':
             continue
-        path3 = path1 + '/credits'
-        params3 = {
-            'api_key': API_KEY,
-            'language': 'ko',
-        }
-        response3 = requests.get(BASE_URL+path3, params=params3)
-        data3_cast = response3.json()['cast']
-        data3_cast_cut = data3_cast[:5]
-        cast_list = []
-        for cast in data3_cast_cut:
-            cast_list.append(cast['original_name'])
-        movie.actors_set = cast_list
-        data3_crew = response3.json()['crew']
-        for crew in data3_crew:
-            if crew['job'] == 'Director':
-                movie.directors_set = crew['original_name']
         movie.save()
+
+        genres = data1['genres']
+        for genre in genres:
+            g1 = Genre(pk=genre['id'], name=genre['name'])
+            g1.save()
+            g1.movies.add(movie)     
     return render(request, 'movies/index.html')
